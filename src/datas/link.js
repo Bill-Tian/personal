9cae4375522b82b6b06fe5a95065f4285a69067a
Mr.T <945119403@qq.com>
2021-12-07 17:46:45 +0800

    link的修改

---
+++ src/datas/link.js
@@ -0,0 +1,121 @@
+/*
+ * @Author: Mr.Tian
+ * @Date: 2021-12-07 15:45:21
+ * @LastEditTime: 2021-12-07 17:43:27
+ * @LastEditors: Mr.Tian
+ * @Description:
+ */
+
+export class linkEnum {
+    static toolsList = [
+        {
+            id: 1,
+            label: "github",
+            des: "代码仓库",
+            link: "https://www.github.com/",
+            url: "https://cdn.jsdelivr.net/gh/Bill-Tian/Picture-library/img/Avatar/Github.webp"
+        },
+        {
+            id: 2,
+            label: "菜鸟教程",
+            des: "提供了编程的基础技术教程, 介绍了HTML、CSS、Javascript、Python,Java,Ruby,C,PHP , MySQL等各种编程语言的基础知识。",
+            link: "https://www.runoob.com/",
+            url: "https://cdn.jsdelivr.net/gh/Bill-Tian/Picture-library/img/Avatar/Github.webp"
+        },
+        {
+            id: 3,
+            label: "Json.cn",
+            des: " JSON在线解析工具",
+            link: "https://www.json.cn/",
+            url: "https://cdn.jsdelivr.net/gh/Bill-Tian/Picture-library/img/Avatar/Github.webp"
+        },
+        {
+            id: 4,
+            label: "BootCDN",
+            des: "稳定、快速、免费的前端开源项目 CDN 加速服务",
+            link: "https://www.bootcdn.cn/",
+            url: "https://cdn.jsdelivr.net/gh/Bill-Tian/Picture-library/img/Avatar/Github.webp"
+        },
+        {
+            id: 4,
+            label: "MDN",
+            des: "Resources for developers, by developers.",
+            link: "https://developer.mozilla.org/",
+            url: "https://cdn.jsdelivr.net/gh/Bill-Tian/Picture-library/img/Avatar/Github.webp"
+        }
+    ];
+    static blogsList = [
+        {
+            id: 1,
+            label: "github",
+            des: "代码仓库",
+            link: "https://www.github.com/",
+            url: "https://cdn.jsdelivr.net/gh/Bill-Tian/Picture-library/img/Avatar/Github.webp"
+        },
+        {
+            id: 2,
+            label: "菜鸟教程",
+            des: "提供了编程的基础技术教程, 介绍了HTML、CSS、Javascript、Python,Java,Ruby,C,PHP , MySQL等各种编程语言的基础知识。",
+            link: "https://www.runoob.com/",
+            url: "https://cdn.jsdelivr.net/gh/Bill-Tian/Picture-library/img/Avatar/Github.webp"
+        },
+        {
+            id: 3,
+            label: "Json.cn",
+            des: " JSON在线解析工具",
+            link: "https://www.json.cn/",
+            url: "https://cdn.jsdelivr.net/gh/Bill-Tian/Picture-library/img/Avatar/Github.webp"
+        },
+        {
+            id: 4,
+            label: "BootCDN",
+            des: "稳定、快速、免费的前端开源项目 CDN 加速服务",
+            link: "https://www.bootcdn.cn/",
+            url: "https://cdn.jsdelivr.net/gh/Bill-Tian/Picture-library/img/Avatar/Github.webp"
+        },
+        {
+            id: 4,
+            label: "MDN",
+            des: "Resources for developers, by developers.",
+            link: "https://developer.mozilla.org/",
+            url: "https://cdn.jsdelivr.net/gh/Bill-Tian/Picture-library/img/Avatar/Github.webp"
+        }
+    ];
+    static studyList = [
+        {
+            id: 1,
+            label: "github",
+            des: "代码仓库",
+            link: "https://www.github.com/",
+            url: "https://cdn.jsdelivr.net/gh/Bill-Tian/Picture-library/img/Avatar/Github.webp"
+        },
+        {
+            id: 2,
+            label: "菜鸟教程",
+            des: "提供了编程的基础技术教程, 介绍了HTML、CSS、Javascript、Python,Java,Ruby,C,PHP , MySQL等各种编程语言的基础知识。",
+            link: "https://www.runoob.com/",
+            url: "https://cdn.jsdelivr.net/gh/Bill-Tian/Picture-library/img/Avatar/Github.webp"
+        },
+        {
+            id: 3,
+            label: "Json.cn",
+            des: " JSON在线解析工具",
+            link: "https://www.json.cn/",
+            url: "https://cdn.jsdelivr.net/gh/Bill-Tian/Picture-library/img/Avatar/Github.webp"
+        },
+        {
+            id: 4,
+            label: "BootCDN",
+            des: "稳定、快速、免费的前端开源项目 CDN 加速服务",
+            link: "https://www.bootcdn.cn/",
+            url: "https://cdn.jsdelivr.net/gh/Bill-Tian/Picture-library/img/Avatar/Github.webp"
+        },
+        {
+            id: 4,
+            label: "MDN",
+            des: "Resources for developers, by developers.",
+            link: "https://developer.mozilla.org/",
+            url: "https://cdn.jsdelivr.net/gh/Bill-Tian/Picture-library/img/Avatar/Github.webp"
+        }
+    ];
+}
